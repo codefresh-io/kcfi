655e1d7a4df97fbf4a624fb9be2ee0d20acf1c05
shirtabachii <shirtabachi@Shirs-MacBook-Pro.local>
2021-10-25 15:01:37 +0300

    update the image list

--- stage/codefresh/images/update-img-list.sh
+++ stage/codefresh/images/update-img-list.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 -bash: /kcfi/stage/codefresg/images/get-img-list.sh --repo prod > images-list
+git config --global user.name shirtabachii
 git commit -am "update the image list"
 git push -u origin CR-7223
