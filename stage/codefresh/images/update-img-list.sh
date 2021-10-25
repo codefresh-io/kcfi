c6b2b9a675fd8997f882e2fae7f8f6d6e3af977b
shirtabachii <shir.tabachi@codefresh.io>
2021-10-25 15:06:47 +0300

    update the image list

--- stage/codefresh/images/update-img-list.sh
+++ stage/codefresh/images/update-img-list.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 -bash: /kcfi/stage/codefresg/images/get-img-list.sh --repo prod > images-list
+git config --global user.email shir.tabachi@codefresh.io
 git config --global user.name shirtabachii
 git commit -am "update the image list"
 git push -u origin CR-7223
