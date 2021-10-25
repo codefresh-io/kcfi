21fe5ec87bfc20afd7307a0ae6abe9529b6ad2d2
shirtabachii <shir.tabachi@codefresh.io>
2021-10-25 15:47:12 +0300

    update the image list

--- stage/codefresh/images/up.sh
+++ stage/codefresh/images/up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
--bash: /kcfi/stage/codefresg/images/get-img-list.sh --repo prod > images-list
+-bash: /kcfi/stage/codefresh/images/get-img-list.sh --repo prod > /kcfi/stage/codefresh/images/images-list
